54a554d225f526a17f56d72855fd2565b5837df7
Kristian Nore <kristian.nore@student.uib.no>
2022-02-17 12:48:15 +0100

    try again

---
+++ src/main/java/game/GameState.java
@@ -0,0 +1,30 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameState {
+	
+	private 
+	private List<Sprite> allSprites;
+	
+	public GameState(Player p) {
+		
+	}
+	
+	
+	
+	public void addSprite() {
+		
+	}
+	
+	public void killSprite() {
+		
+	}
+	
+	public ArrayList getAllSprites() {
+		
+		
+		return null; 
+	}
+}
