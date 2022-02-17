20d69969e4a594ac06360d1189beb018777596bd
saraa <saraa@10.111.5.146>
2022-02-17 13:10:15 +0100

    Sprite  interface documentation

--- src/main/java/game/Sprite.java
+++ src/main/java/game/Sprite.java
@@ -4,18 +4,35 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
-//TODO Document
+/**
+ * An interface that determines the behaviours and variables of all sprites in the game. 
+ * 
+ * @author saraa
+ *
+ */
 public interface Sprite {
-	
+	/**
+	 * @return the string symbol that represents the sprite
+	 */
 	public String getSymbol();
-	
+	/**
+	 * @return the sprite's integer position in the x-axis
+	 */
 	public int getX();
-	
+	/**
+	 * @return the sprite's integer position in the y-axis
+	 */
 	public int getY();
-	
+	/**
+	 * draws the sprite onto the board
+	 */
 	public void draw(SpriteBatch batch, BitmapFont font);
-	
+	/**
+	 * updates the sprite's stat's as the game renders
+	 */
 	public void update();
-	
+	/**
+	 * moves the sprite's position as the game renders
+	 */
 	public void move();
 }
