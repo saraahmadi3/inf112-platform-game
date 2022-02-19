2cbf118f7956cbea6792c42671a66e4a31fa625d
Kristian Nore <kristian.nore@student.uib.no>
2022-02-19 02:07:09 +0100

    Small rearrangements.

--- src/main/java/game/GameState.java
+++ src/main/java/game/GameState.java
@@ -16,37 +16,11 @@ public class GameState {
 		waitingRemovalSprites = new ArrayList<GameObjects>();
 		allPlatforms = new ArrayList<Platform>();
 
-		
-		new Tips(this);
-		new Door(this, 1050, 650, 20, 34);
-		new Key(this, 1050, 12);
-		player1 = new Player(50, 15, this);
-	
-		new Platform(this, -500, -10, 2000, 20, "grass.png"); //Floor
-		new Platform(this, -500, 718, 2000, 20); //Top
-		new Platform(this, -10, -500, 12, 2000); //Left wall
-		new Platform(this, 1078, -500, 15, 2000); //Right wall
-		
-		
-		new MovingPlatform(this, 600, 150, 100, 8, 100, 0, 50);
-		new MovingPlatform(this, 800, 250, 150, 8, 0, 150, 75);
-		new MovingPlatform(this, 150, 620, 75, 8, 420, 0, 75);
-		
-		//TODO fix bug where player is not always grounded on diagonally moving platforms
-		//new MovingPlatform(this, 400, 360, 75, 8, 250, 75, 25); 
-		new MovingPlatform(this, 400, 360, 75, 8, 250, 0, 50); 
-		
-		new BoostPlatform(this, 30, 250, 50, 8, 2);
-		new GhostPlatform(this, 750,600,100,8,2);
-		
-		new Platform(this, 250,400,75,8);
-		new Platform(this, 200,50,100, 8);
-		new Platform(this, 350,10,200,170);
-		new Platform(this, 28,600,54,8);
-		new Platform(this, 980,642,500,8);
+		level1();
 		
 	}
 	
+
 	//Adds the sprite in waitlist to be added to the main list when allowed, avoids ConcurrentModificationException.
 	public void addSprite (GameObjects s) {
 		waitingSprites.add(s);
@@ -100,4 +74,36 @@ public class GameState {
 	public void levelComplete() {
 		new Text(this, 800, 600, "Level complete. TODO: Make next level");
 	}
+	
+	//TODO find a better place for this information.
+	private void level1() {
+		new Tips(this);
+		new Door(this, 1050, 650, 20, 34);
+		new Key(this, 1050, 12);
+		player1 = new Player(50, 15, this);
+	
+		new Platform(this, -500, -10, 2000, 20, "grass.png"); //Floor
+		new Platform(this, -500, 718, 2000, 20); //Top
+		new Platform(this, -10, -500, 12, 2000); //Left wall
+		new Platform(this, 1078, -500, 15, 2000); //Right wall
+		
+		
+		new MovingPlatform(this, 600, 150, 100, 8, 100, 0, 50);
+		new MovingPlatform(this, 800, 250, 150, 8, 0, 150, 75);
+		new MovingPlatform(this, 150, 620, 75, 8, 420, 0, 75);
+		
+		//TODO fix bug where player is not always grounded on diagonally moving platforms
+		//new MovingPlatform(this, 400, 360, 75, 8, 250, 75, 25); 
+		new MovingPlatform(this, 400, 360, 75, 8, 250, 0, 50); 
+		
+		new BoostPlatform(this, 30, 250, 50, 8, 2);
+		new GhostPlatform(this, 750,600,100,8,2);
+		
+		new Platform(this, 250,400,75,8);
+		new Platform(this, 200,50,100, 8);
+		new Platform(this, 350,10,200,170);
+		new Platform(this, 28,600,54,8);
+		new Platform(this, 980,642,500,8);
+	}
+	
 }
--- src/test/java/tests_oblig1/PlayerTest.java
+++ src/test/java/tests_oblig1/PlayerTest.java
@@ -7,6 +7,10 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+import game.GameLoop;
 import game.GameObjects;
 import game.GameState;
 import game.Player;
@@ -23,6 +27,17 @@ class PlayerTest {
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws NullPointerException {
+		
+		//Probably can't test much without running the GameLoop which is what makes everything happen. 
+		//Starting the GameLoop would probably look something like the code below, but it might be better to find a way
+		//to start a fresh GameLoop without a window and without the level designs. Perhaps finding a way to call render() from 
+		//GameLoop manually would be a good solution, since we wouldn't have to deal with the Lwjgl3 stuff more than necessary.
+		/*
+		Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+		cfg.setWindowedMode(1080, 720);
+		new Lwjgl3Application(new GameLoop(), cfg);
+		*/
+		
 		playerOne = new Player(10, (int) FLOOR, state);
 		state = new GameState();
 		
