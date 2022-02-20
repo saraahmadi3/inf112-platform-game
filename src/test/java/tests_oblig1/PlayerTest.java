3746e580f0aa9014a039bb2c286508ed59babd43
Adrian Clopan <adrian.clopan@student.uib.no>
2022-02-20 12:51:56 +0100

    Worked on debugging PlayerTest. Running graphics is too difficult to work around. Need a backend solution for testing without running GUI.

--- src/test/java/tests_oblig1/PlayerTest.java
+++ src/test/java/tests_oblig1/PlayerTest.java
@@ -22,33 +22,57 @@ class PlayerTest {
 	private static final double G = 150; //Gravity acceleration
 	private static final double FLOOR = 12.5; //The Y-coordinate at which there is a floor
 	
-	private static Player playerOne;
-	private static GameState state;
-	private static GameLoop loop;
+	static GameLoop loop = new GameLoop();
+	static GameState state;
+	static Player playerOne;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws NullPointerException {
+		
+		Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+		cfg.setTitle("PlayerTest");
+		cfg.setWindowedMode(1080, 720);
+		
+		new Lwjgl3Application(loop, cfg);
 		
 		//Probably can't test much without running the GameLoop which is what makes everything happen. 
 		//Starting the GameLoop would probably look something like the code below, but it might be better to find a way
 		//to start a fresh GameLoop without a window and without the level designs. Perhaps finding a way to call render() from 
 		//GameLoop manually would be a good solution, since we wouldn't have to deal with the Lwjgl3 stuff more than necessary.
-		
-		loop = new GameLoop();
-		Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
-		cfg.setTitle("PlayerTest");
-		cfg.setWindowedMode(1080, 720);
 		state = loop.getGame();
-		playerOne = new Player(10, (int) FLOOR, state);
+		ArrayList<GameObjects> sprites = state.getAllSprites();
+		
+		for (GameObjects sprite : sprites) {
+			state.killSprite(sprite);
+		}
+		
+		try {
+			playerOne = new Player(10, (int) FLOOR, state);
+		} catch (NullPointerException e) {
+			// TODO: handle exception
+			
+		}
+		
 		state.addSprite(playerOne);
-		new Lwjgl3Application(loop, cfg);
+		
+		
+		
 		
 	}
 
 	@Test
 	void playerSpawStaysFloor() throws InterruptedException {
-		Thread.sleep(1000);
 		assertEquals((int) FLOOR, playerOne.getY());
+	}
+	
+	@Test
+	void playerOnlySprite() throws InterruptedException {
+		assertEquals(1, state.getAllSprites().size());
+	}
+	
+	@Test 
+	void playerAddedIsPlayer1() {
+		assertEquals(playerOne, state.getPlayer1());
 	}
 
 }
