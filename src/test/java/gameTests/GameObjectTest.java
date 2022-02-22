667b235d8f9008929f53d0bcce272059d4d17e5c
Adrian Clopan <adrian.clopan@student.uib.no>
2022-02-22 21:04:09 +0100

    Refined to-do comments in GameObjectTest. Issue #13.

--- src/test/java/gameTests/GameObjectTest.java
+++ src/test/java/gameTests/GameObjectTest.java
@@ -6,22 +6,81 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import game.BoostPlatform;
+import game.Door;
+import game.Enemy;
 import game.GameState;
+import game.GhostPlatform;
+import game.Key;
+import game.MovingPlatform;
+import game.Platform;
+import game.Player;
+import game.Text;
+import game.Tips;
 
 /**
  * @author adria
  * Tests to confirm that AbstractObjects' methods are working as intended
  */
+
+//For this class (AbstractObject) these methods can be tested using any class 
+//        that extends AbstractObject (without the methods being overwritten):
+
+//checkForHits(), setGameState(), getGameState(), getSymbol(), getType(), getX(), 
+//getY(), getXMid(), getYMid(), setX(), setY(), setXAndY(), moveByX(), moveByY(), 
+//moveByXandY(), getWidth(), getHeight(), setWidth(), setHeight()
 class GameObjectTest {
 	
 	private static GameState game;
-	private static BoostPlatform boost;
-	private static 
+	private static Player player;
+	private static BoostPlatform boostP;
+	private static Door door;
+	private static Enemy enemy;
+	private static GhostPlatform ghostP;
+	private static Key key;
+	private static MovingPlatform movingP;
+	private static Platform regularP;
+	private static Text text;
+	private static Tips tip;
 	
-	@BeforeEach
-	void setUp() throws Exception {
-		return;
-	}
+	//TO-DO: 
+//Each of the following methods listed by class should each have at least one test 
+	//confirming they are working as intended.
+//BoostPlatform:
+//update()
+//checkForBoost()
+
+//Door:
+//update()
+//checkForKey()
+//openDoor()
+
+//Enemy:
+//update()
+//move()
+
+//GhostPlatform:
+//update()
+//checkForPlayer()
+//checkForHit()
+
+//Key:
+//update()
+//checkForPlayer()
+
+//MovingPlatform:
+//update()
+//move()
+//movePlayer()
+
+//Platform:
+//getType()
+
+//Text:
+//getSymbol()
+
+//Tips:
+//move()
+	
 
 	@Test
 	void test() {
