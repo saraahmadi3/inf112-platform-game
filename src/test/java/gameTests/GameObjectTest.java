5e9fba9cb9ff129387f708fee218fe1b61b34016
Adrian Clopan <adrian.clopan@student.uib.no>
2022-02-22 20:40:30 +0100

    Created GameObjectTest for various AbstractObject method testing cases. Issue #13

---
+++ src/test/java/gameTests/GameObjectTest.java
@@ -0,0 +1,31 @@
+package gameTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import game.BoostPlatform;
+import game.GameState;
+
+/**
+ * @author adria
+ * Tests to confirm that AbstractObjects' methods are working as intended
+ */
+class GameObjectTest {
+	
+	private static GameState game;
+	private static BoostPlatform boost;
+	private static 
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		return;
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
