ebcd393c9bfec3cbbd130260cebef687983ea9b1
saraa <saraa@10.111.7.250>
2022-02-22 13:54:30 +0100

    added an enemy object that moves on a platform

---
+++ src/main/java/game/Enemy.java
@@ -0,0 +1,58 @@
+package game;
+
+
+
+public class Enemy extends AbstractObject {
+	
+
+	private double speed = 10;
+	private boolean goingRight;
+	private Platform platform;
+	
+	
+	
+	public Enemy(GameState game, Platform platform) {
+		this.platform = platform;
+		super.setGameState(game);
+		game.addSprite(this);
+		
+		super.setX(platform.getXMid());
+		super.setY(platform.getY()+platform.getHeight());
+		goingRight=true;
+		
+		super.setHeight(16);
+		super.setWidth(16);
+		super.setSprite("player.png");
+		
+	}
+	
+	public void update() {
+		move();
+//		if() collision med player enten killsprite/gameover
+	}
+	
+	public void move() {
+		double delta = super.getGameState().getDeltaTime();
+		
+		if(goingRight) {
+			if(super.getX()+super.getWidth() > platform.getX()+platform.getWidth()) {
+				goingRight = false;
+			} else {
+				moveByX(delta*speed);
+				super.setY(platform.getY()+platform.getHeight());
+			}
+		}
+		
+		if(!goingRight) {
+			if(super.getX() < platform.getX()) {
+				goingRight = true;
+			} else {
+				moveByX(-delta*speed);
+				super.setY(platform.getY()+platform.getHeight());
+			}
+		}
+			
+		
+	}
+	
+}
