ad3674dd6e560272103b1ed92d5037cd77b9e137
Adrian Clopan <adrian.clopan@student.uib.no>
2022-02-24 13:22:06 +0100

    Issue #19. Implemented boostPlatformTest().

--- src/test/java/gameTests/GameObjectTest.java
+++ src/test/java/gameTests/GameObjectTest.java
@@ -2,9 +2,12 @@ package gameTests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.JUnitException;
 
+import exceptions.InvalidPlatformException;
 import game.BoostPlatform;
 import game.Door;
 import game.Enemy;
@@ -36,45 +39,153 @@ import game.Tips;
 class GameObjectTest {
 	
 	private static GameState game;
-	private static Player player;
+	private static Player playerOne;
+	private static Player playerTwo;
 	private static BoostPlatform boostP;
+	private static BoostPlatform negativeBoostP;
 	private static Door door;
-	private static Enemy enemy;
+	private static Enemy boostPlatEnemy;
+	private static Enemy movingPlatEnemy;
+	private static Enemy regularPlatEnemy;
+	private static Enemy ghostPlatEnemy;
 	private static GhostPlatform ghostP;
 	private static Key key;
 	private static MovingPlatform movingP;
 	private static Platform regularP;
+	private static Text tooLong;
 	private static Text text;
 	private static Tips tip;
+	
+
+	private static final double J = 150;
+	private static double boostFactor = 2;
+	private static double negativeBoostFactor = -1;
+	private static double ghostDelay = 2;
+	private static int xRangeMovingP = 250;
+	private static int yRangeMovingP = 0;
+	private static int speedMovingP = 50;
+	
+	@BeforeAll
+	static void setUp() {
+		game = new GameState(0);
+		playerOne = new Player(50, 15, game, "testMode", 1);
+		playerTwo = new Player(50, 15, game, "testMode", 2);
+		
+		negativeBoostP = new BoostPlatform(game, -500, -10, 1150, 20, negativeBoostFactor, "testMode"); 
+		boostP = new BoostPlatform(game, -500, -10, 1150, 20, boostFactor, "testMode"); 
+		ghostP = new GhostPlatform(game, -500, -10, 1150, 20, ghostDelay, "testMode");
+		movingP = new MovingPlatform(game, -500, -10, 1150, 20, xRangeMovingP, yRangeMovingP, speedMovingP, "testMode");
+		door = new Door(game, 60, 15, 20, 34, "testMode");
+		
+		regularP = new Platform(game, -500, -10, 1150, 20, "testMode");
+		boostPlatEnemy = new Enemy(game, boostP, "testMode");
+		movingPlatEnemy = new Enemy(game, movingP, "testMode");
+		regularPlatEnemy = new Enemy(game, regularP, "testMode");
+		ghostPlatEnemy = new Enemy(game, ghostP, "testMode");
+		key = new Key(game, 55, 15, "testMode");
+		text = new Text(game, 30, 200, "This is a regular text at x=30 and y=200");
+		tooLong = new Text(game, 30, 300, "This text is too l" + " ".repeat(10000) + "ng");
+		Tips tip = new Tips(game);
+		
+	}
+	
 	
 // Approach: SPLIT TESTS INTO CATEGORIES. Create a new issue in git for each category.
 
 	//Example:
-	//==========TO-DO=========== (Issue #nr)
+	//==========TO-DO=========== (Issue #nr)[OPEN / CLOSED]
 	//ToDoClass: toDo(), example()
 	
 		//All issues link to #13
 	
 	
-//=========PLATFORMS========(Issue #19)
-//BoostPlatform: update(), checkForBoost()
+//=========PLATFORMS========(Issue #19)[OPEN]
+	@Test 
+	void boostPlatformTest() {
+		assertEquals(0, playerOne.getGv());
+		game.addSprite(boostP);
+		game.addSprite(playerOne);
+		game.addAllNewSprites();
+		
+		//Adding playerTwo to wait list
+		game.addSprite(playerTwo);
+		
+		playerOne.move();
+		assertTrue(boostP.checkForHit(playerOne));
+		assertFalse(boostP.checkForHit(playerTwo));
+		
+		//checkForBoost() in BoostPlatform
+		boostP.checkForBoost(1);
+		assertEquals(-J*boostFactor, playerOne.getGv());
+		
+		//update() in BoostPlatform
+		playerOne.boost(0);
+		boostP.update();
+		assertEquals(-J*boostFactor, playerOne.getGv());
+		
+		game.killSprite(playerOne);
+		game.removeAllDeadSprites();
+		
+		//Adding playerTwo from wait list
+		game.addAllNewSprites();
+		playerTwo.move();
+		
+		assertTrue(boostP.checkForHit(playerTwo));
+		
+		//checkForBoost() playerTwo
+		boostP.checkForBoost(2);
+		assertEquals(-J*boostFactor, playerTwo.getGv());
+		
+		//update() boost with playerTwo on it
+		playerTwo.boost(0);
+		boostP.update();
+		assertEquals(-J*boostFactor, playerTwo.getGv());
+		
+		game.killSprite(boostP);
+		game.removeAllDeadSprites();
+		
+		negativeBoostP = BoostPlatform(game, -500, -10, 1150, 20, negativeBoostFactor, "testMode"); 
+		
+		assertThrows(InvalidPlatformException.class, () -> {
+			game.addSprite(negativeBoostP);
+		}, "InvalidPlatformException expected. Cannot add BoostPlatform with negative boost.");
+		
+		game.addAllNewSprites();
+		assertFalse(game.getAllPlatforms().contains(negativeBoostP));
+		if(game.getAllPlatforms().contains(negativeBoostP)) {
+			game.killSprite(negativeBoostP);
+			game.removeAllDeadSprites();
+		}
+		
+		assertThrows(InvalidPlatformException.class, () -> {
+			game.addSpriteQ(negativeBoostP);
+		}, "InvalidPlatformException expected. Cannot add BoostPlatform with negative boost.");
+		
+		assertFalse(game.getAllPlatforms().contains(negativeBoostP));
+	}
+	
+	
 //GhostPlatform: update(), checkForPlayer(), checkForHit()
+	//Need to test fall when player stands too long on GhostPlatform
+	//Need to find conditions for player to pass through. Check for negative delta Y.
+	
 //MovingPlatform: update(), move(), movePlayer()
 //Platform: getType()
+	
 
 	
-//======DOOR-KEY-PLAYER======(Issue #20)
+//======DOOR-KEY-PLAYER======(Issue #20)[OPEN]
 	//(Say it fast, it becomes dorky player lol)
 //Door: update(), checkForKey(), openDoor()
 //Key(): update(), checkForPlayer()
 
 	
-//=======TIPS-and-TEXT========(Issue #21)
+//=======TIPS-and-TEXT========(Issue #21)[OPEN]
 //Tips: move()
 //Text: getSymbol()
 
 	
-//===========ENEMY===========(Issue #22)
+//===========ENEMY===========(Issue #22)[OPEN]
 //Enemy: update(), move()
 	
 
